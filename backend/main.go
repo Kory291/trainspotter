9187da6c48cb61bdbaa5a672507f89a5357bdb36
Kory291 <kory291@proton.me>
2026-07-12 23:52:36 +0200

    apply formatting

--- backend/main.go
+++ backend/main.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-    "github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/chi/v5/middleware"
 
 	"trainspotter-backend/internal/api"
 )
@@ -16,10 +16,9 @@ var routes = func(r chi.Router) {
 	r.Post("/sightings", api.PostSighting)
 }
 
-
 func main() {
-    r := chi.NewRouter()
-    r.Use(middleware.Logger)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 	routes(r)
-    http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":8080", r)
 }
